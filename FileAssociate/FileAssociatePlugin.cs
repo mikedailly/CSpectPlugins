25455b088a29a52361dee5d551471c95cac98e52
Mike Dailly <mike@dailly.org>
2022-12-29 14:00:05 +0000

    + Added file association (for windows) plugin - using ALT+CTRL+SHIFT+A to associate

---
+++ FileAssociate/FileAssociatePlugin.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using Microsoft.Win32;
+using Plugin;
+
+namespace FileAssociate
+{
+    // *********************************************************************************************************
+    /// <summary>
+    ///     The copper disassembler
+    /// </summary>
+    // *********************************************************************************************************
+    class FileAssociationPlugin : iPlugin
+    {
+        /// <summary>CSpect emulator interface</summary>
+        iCSpect CSpect;
+
+        // *********************************************************************************************************
+        /// <summary>
+        ///     Init the plugin
+        /// </summary>
+        /// <param name="_CSpect">CSpect interface</param>
+        /// <returns>
+        ///     A list of plugin stuff
+        /// </returns>
+        // *********************************************************************************************************
+        public List<sIO> Init(iCSpect _CSpect)
+        {
+            Debug.WriteLine("Copper Disassembler added");
+
+            CSpect = _CSpect;
+
+            // Detect keypress for starting disassembler
+            List<sIO> ports = new List<sIO>();
+
+            // Only on windows
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                ports.Add(new sIO("<ctrl><alt><shift>a", eAccess.KeyPress, 0));     // Add file assosiation
+            }
+            return ports;
+        }
+
+        // ******************************************************************************************
+        /// <summary>
+        ///     Key pressed callback.
+        /// </summary>
+        /// <param name="_id"></param>
+        /// <returns></returns>
+        // ******************************************************************************************
+        public bool KeyPressed(int _id)
+        {
+            if (_id == 0)
+            {
+                RegFunctions r = new RegFunctions();
+                r.EnsureAssociationsSet();
+            }
+            return false;
+        }
+
+        // ******************************************************************************************
+        /// <summary>
+        ///     Showdown - free any non-managed resources
+        /// </summary>
+        // ******************************************************************************************
+        public void Quit()
+        {
+        }
+
+        // ******************************************************************************************
+        /// <summary>
+        ///     Read access type - not used
+        /// </summary>
+        /// <param name="_type"></param>
+        /// <param name="_address"></param>
+        /// <param name="_isvalid"></param>
+        /// <returns></returns>
+        // ******************************************************************************************
+        public byte Read(eAccess _type, int _address, int _id, out bool _isvalid)
+        {
+            _isvalid = false;
+            return 0;
+        }
+
+        // ******************************************************************************************
+        /// <summary>
+        ///     Machine has been reset
+        /// </summary>
+        // ******************************************************************************************
+        public void Reset()
+        {
+        }
+
+        // ******************************************************************************************
+        /// <summary>
+        ///     Called once an emulator frame - update copper if "Active"
+        /// </summary>
+        // ******************************************************************************************
+        public void Tick()
+        {
+        }
+
+        // ******************************************************************************************
+        /// <summary>
+        ///     Write access type - not used
+        /// </summary>
+        /// <param name="_type"></param>
+        /// <param name="_port"></param>
+        /// <param name="_value"></param>
+        /// <returns></returns>
+        // ******************************************************************************************
+        public bool Write(eAccess _type, int _port, int _id, byte _value)
+        {
+            return false;
+        }
+
+
+    }
+}
---
+++ FileAssociate/RegFunctions.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileAssociate
+{
+    // ************************************************************************************************************
+    /// <summary>
+    ///     The registry functions are in a seperate class, so that creating the plugin
+    ///     shouldn't crash Linux/Mac, as the windows registry dependancies should only 
+    ///     be loaded when THIS class is created.
+    /// </summary>
+    // ************************************************************************************************************
+    public class RegFunctions
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+        private const int SHCNE_ASSOCCHANGED = 0x8000000;
+        private const int SHCNF_FLUSH = 0x1000;
+
+
+        // ****************************************************************************************************************
+        /// <summary>
+        ///     Set the .NEX file assosoation
+        /// </summary>
+        // ****************************************************************************************************************
+        public void EnsureAssociationsSet()
+        {
+            var filePath = Process.GetCurrentProcess().MainModule.FileName;
+            bool madeChanges1 = SetAssociation(".nex", "NEX_Program_File", "ZX Spectrum Next program", filePath);
+            bool madeChanges2 = SetAssociation(".snx", "SNX_Program_File", "ZX Spectrum Next program", filePath);
+            if (madeChanges1 || madeChanges2)
+            {
+                SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSH, IntPtr.Zero, IntPtr.Zero);
+                MessageBox.Show(".NEX and .SNX file association added.", "File Association Plugin");
+            }
+        }
+
+        // ****************************************************************************************************************
+        /// <summary>
+        ///     Set file assosiation for windows
+        /// </summary>
+        /// <param name="extension">The extension to setup</param>
+        /// <param name="progId">program id</param>
+        /// <param name="fileTypeDescription">long description</param>
+        /// <param name="applicationFilePath">path to exe to bind to</param>
+        /// <returns>
+        ///     True for okay, False for error
+        /// </returns>
+        // ****************************************************************************************************************
+        public bool SetAssociation(string extension, string progId, string fileTypeDescription, string applicationFilePath)
+        {
+            bool madeChanges = false;
+            madeChanges |= SetKeyDefaultValue(@"Software\Classes\" + extension, progId);
+            madeChanges |= SetKeyDefaultValue(@"Software\Classes\" + progId, fileTypeDescription);
+            madeChanges |= SetKeyDefaultValue($@"Software\Classes\{progId}\shell\open\command", "\"" + applicationFilePath + "\" \"%1\"");
+
+            var CurrentUser = Registry.ClassesRoot.OpenSubKey(".nex\\DefaultIcon", true);
+            if (CurrentUser == null) CurrentUser = Registry.ClassesRoot.CreateSubKey(".nex\\DefaultIcon");
+            CurrentUser.SetValue("", applicationFilePath + "\\ZXNext.ico");
+
+            return madeChanges;
+        }
+
+        // ****************************************************************************************************************
+        /// <summary>
+        ///     Set a key/value pair into the registry
+        /// </summary>
+        /// <param name="keyPath">the key we're setting</param>
+        /// <param name="value">the value to set</param>
+        /// <returns>
+        ///     true for set okay, false for error
+        /// </returns>
+        // ****************************************************************************************************************
+        private bool SetKeyDefaultValue(string keyPath, string value)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(keyPath))
+            {
+                if (key.GetValue(null) as string != value)
+                {
+                    key.SetValue(null, value);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
